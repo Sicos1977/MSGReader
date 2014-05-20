23b19ec8e8d90e3561aeed17095301c7f2e79a3b
sicos1977 <sicos2002@hotmail.com>
2014-05-20 19:48:21 +0200

    Finished MSGMapper

--- MsgMapper/MainForm.Designer.cs
+++ MsgMapper/MainForm.Designer.cs
@@ -33,90 +33,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.AddFolderButton = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Folder = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.StatusListBox = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Folder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FoldersDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.FoldersDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // AddFolderButton
             // 
-            this.AddFolderButton.Location = new System.Drawing.Point(462, 207);
-            this.AddFolderButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AddFolderButton.Location = new System.Drawing.Point(924, 409);
+            this.AddFolderButton.Margin = new System.Windows.Forms.Padding(4);
             this.AddFolderButton.Name = "AddFolderButton";
-            this.AddFolderButton.Size = new System.Drawing.Size(70, 26);
+            this.AddFolderButton.Size = new System.Drawing.Size(140, 50);
             this.AddFolderButton.TabIndex = 0;
             this.AddFolderButton.Text = "Add folder";
             this.AddFolderButton.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Folder});
-            this.dataGridView1.Location = new System.Drawing.Point(8, 19);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(524, 184);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // Folder
-            // 
-            this.Folder.HeaderText = "Folder";
-            this.Folder.Name = "Folder";
-            this.Folder.Width = 500;
+            this.AddFolderButton.Click += new System.EventHandler(this.AddFolderButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 5);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.Size = new System.Drawing.Size(176, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Folders to watch:";
             // 
             // StatusListBox
             // 
             this.StatusListBox.FormattingEnabled = true;
-            this.StatusListBox.Location = new System.Drawing.Point(8, 260);
-            this.StatusListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.StatusListBox.ItemHeight = 25;
+            this.StatusListBox.Location = new System.Drawing.Point(16, 525);
+            this.StatusListBox.Margin = new System.Windows.Forms.Padding(4);
             this.StatusListBox.Name = "StatusListBox";
-            this.StatusListBox.Size = new System.Drawing.Size(524, 160);
+            this.StatusListBox.Size = new System.Drawing.Size(1044, 279);
             this.StatusListBox.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 245);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(13, 496);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(79, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Status:";
             // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(23, 409);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(894, 62);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "This program uses a filesystemwatcher to monitor folders for Outlook Message File" +
+    "s. Click the [Add Folder] button to add a folder to watch.";
+            // 
+            // Folder
+            // 
+            this.Folder.HeaderText = "Folder";
+            this.Folder.Name = "Folder";
+            this.Folder.ReadOnly = true;
+            this.Folder.Width = 500;
+            // 
+            // FoldersDataGridView
+            // 
+            this.FoldersDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.FoldersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FoldersDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Folder});
+            this.FoldersDataGridView.Location = new System.Drawing.Point(16, 37);
+            this.FoldersDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.FoldersDataGridView.MultiSelect = false;
+            this.FoldersDataGridView.Name = "FoldersDataGridView";
+            this.FoldersDataGridView.ReadOnly = true;
+            this.FoldersDataGridView.RowTemplate.Height = 33;
+            this.FoldersDataGridView.Size = new System.Drawing.Size(1048, 354);
+            this.FoldersDataGridView.TabIndex = 1;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 426);
+            this.ClientSize = new System.Drawing.Size(1078, 819);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.StatusListBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.FoldersDataGridView);
             this.Controls.Add(this.AddFolderButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MSG properties mapper";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.FoldersDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,11 +146,12 @@
         #endregion
 
         private System.Windows.Forms.Button AddFolderButton;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox StatusListBox;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Folder;
+        private System.Windows.Forms.DataGridView FoldersDataGridView;
     }
 }
 
