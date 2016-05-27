f173966993e13a5707580baa80cb4bfdbdef6317
sicos1977 <sicos2002@hotmail.com>
2016-05-27 19:56:17 +0200

    Remove unused using

--- MsgReader/Outlook/Message.cs
+++ MsgReader/Outlook/Message.cs
@@ -9,7 +9,6 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.Pkcs;
 using System.Security.Cryptography.X509Certificates;
-using System.Security.Policy;
 using System.Text;
 using MsgReader.Exceptions;
 using MsgReader.Helpers;
