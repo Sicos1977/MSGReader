559694834ae87ce8f496bd9a4ff335a880643a6e
sicos1977 <sicos2002@hotmail.com>
2016-06-21 20:03:23 +0200

    Added comments to AddressBookEntryIdType

--- MsgReader/Outlook/UnsendableRecipients.cs
+++ MsgReader/Outlook/UnsendableRecipients.cs
@@ -107,16 +107,59 @@ namespace MsgReader.Outlook
     /// </summary>
     public enum AddressBookEntryIdType
     {
+        /// <summary>
+        /// A local mail user
+        /// </summary>
         LocalMailUser = 0x00000000,
+
+        /// <summary>
+        /// A distribution list
+        /// </summary>
         DistributionList = 0x00000001,
+
+        /// <summary>
+        /// A bulletinboard or public folder
+        /// </summary>
         BulletinBoardOrPublicFolder = 0x00000002,
+
+        /// <summary>
+        /// An automated mailbox
+        /// </summary>
         AutomatedMailBox = 0x00000003,
+
+        /// <summary>
+        /// An organiztional mailbox
+        /// </summary>
         OrganizationalMailBox = 0x00000004,
+
+        /// <summary>
+        /// A private distribtion list
+        /// </summary>
         PrivateDistributionList = 0x00000005,
+
+        /// <summary>
+        /// A remote mail user
+        /// </summary>
         RemoteMailUser = 0x00000006,
+
+        /// <summary>
+        /// A container
+        /// </summary>
         Container = 0x00000100,
+
+        /// <summary>
+        /// A template
+        /// </summary>
         Template = 0x00000101,
+
+        /// <summary>
+        /// One off user
+        /// </summary>
         OneOffUser = 0x00000102,
+
+        /// <summary>
+        /// Search
+        /// </summary>
         Search = 0x00000200
     }
     #endregion
