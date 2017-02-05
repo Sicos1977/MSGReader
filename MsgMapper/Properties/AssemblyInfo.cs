05120dfa831677e8f86c0ca8084cb038b0666791
sicos1977 <sicos2002@hotmail.com>
2017-02-05 16:23:25 +0100

    Upped version number

--- MsgMapper/Properties/AssemblyInfo.cs
+++ MsgMapper/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MsgMapper")]
-[assembly: AssemblyCopyright("Copyright © 2013 - 2016 Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2017 Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.8.0")]
-[assembly: AssemblyFileVersion("2.0.8.0")]
+[assembly: AssemblyVersion("2.0.9.0")]
+[assembly: AssemblyFileVersion("2.0.9.0")]
