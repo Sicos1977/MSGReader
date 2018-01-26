9ec031d0f94781d73acfb543eede40f0832f993a
sicos1977 <sicos2002@hotmail.com>
2018-01-26 19:24:00 +0100

    Added unit test to do heavy load extraction

--- MsgReaderTests/BasicContentTests.cs
+++ MsgReaderTests/BasicContentTests.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MsgReader;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace MsgReaderTests
 {
---
+++ MsgReaderTests/LoadTest.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsgReader;
+
+namespace MsgReaderTests
+{
+    [TestClass]
+    public class LoadTest
+    {
+        private DirectoryInfo _tempDirectory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            _tempDirectory = Directory.CreateDirectory(tempDirectory);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _tempDirectory.Delete(true);
+        }
+
+        [TestMethod]
+        public void Extract_10000_Times()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                var msgReader = new Reader();
+                var tempDirectory = Directory.CreateDirectory(Path.Combine(_tempDirectory.FullName, Path.GetRandomFileName()));
+                msgReader.ExtractToFolder("SampleFiles\\EmailWithAttachments.msg", tempDirectory.FullName);
+            }
+        }
+    }
+}
