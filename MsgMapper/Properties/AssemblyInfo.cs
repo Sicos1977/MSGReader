79823507c54d2fb27dae183bf8be9aa3539148ae
sicos1977 <sicos2002@hotmail.com>
2018-07-12 18:26:08 +0200

    Updated version number and copyright information

--- MsgMapper/Properties/AssemblyInfo.cs
+++ MsgMapper/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MsgMapper")]
-[assembly: AssemblyCopyright("Copyright © 2013 - 2017 Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2018 Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.3.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
--- MsgViewer/ViewerForm.cs
+++ MsgViewer/ViewerForm.cs
@@ -11,7 +11,7 @@ using MsgViewer.Helpers;
 using MsgViewer.Properties;
 
 /*
-   Copyright 2013-2016 Kees van Spelde
+   Copyright 2013-2018 Kees van Spelde
 
    Licensed under The Code Project Open License (CPOL) 1.02;
    you may not use this file except in compliance with the License.
