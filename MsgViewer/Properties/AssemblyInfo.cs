7e34695812a17627980f347c245bae7861747556
sicos1977 <sicos2002@hotmail.com>
2019-11-21 16:39:53 +0100

    Upped version number

--- MsgViewer/Properties/AssemblyInfo.cs
+++ MsgViewer/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MsgViewer")]
-[assembly: AssemblyCopyright("Copyright © 2013 - 2017 Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2019 Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
