f9594e885ab45a606f3f22a0a487ec9dece1da2a
Kees <magic-sessions@home.nl>
2019-12-02 10:10:01 +0100

    Added test to check if stream is valid after removing attachments from msg file

---
+++ MsgReaderTests/RemoveAttachmentTests.cs
@@ -0,0 +1,35 @@
+﻿
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsgReader.Outlook;
+
+namespace MsgReaderTests
+{
+    [TestClass]
+    public class RemoveAttachmentTests
+    {
+        [TestMethod]
+        public void RemoveAttachments()
+        {
+            using (var inputStream = File.OpenRead(Path.Combine("SampleFiles", "EmailWith2Attachments.msg")))
+            using (var inputMessage = new Storage.Message(inputStream, FileAccess.ReadWrite))
+            {
+                var attachments = inputMessage.Attachments.ToList();
+
+                foreach (var attachment in attachments)
+                    inputMessage.DeleteAttachment(attachment);
+
+                using (var outputStream = new MemoryStream())
+                {
+                    inputMessage.Save(outputStream);
+                    using (var outputMessage = new Storage.Message(outputStream))
+                    {
+                        var count = outputMessage.Attachments.Count;
+                        Assert.IsTrue(count == 0);
+                    }
+                }
+            }
+        }
+    }
+}
