4fc4ccd9ca0126a573dd577d117e92e5dd647918
Kees <magic-sessions@home.nl>
2021-09-24 19:02:09 +0200

    Add method to repair base64 strings with invalid chars in it

--- MsgReaderCore/Mime/Decode/Base64.cs
+++ MsgReaderCore/Mime/Decode/Base64.cs
@@ -21,10 +21,20 @@ namespace MsgReader.Mime.Decode
 	        {
                 return Convert.FromBase64String(base64Encoded);
             }
-	        catch (Exception)
+	        catch (Exception exception)
 	        {
-				Logger.WriteToLog("Base64 deconding failed");
-	            return new byte[0];
+				Logger.WriteToLog($"Base64 decoding failed, error: {ExceptionHelpers.GetInnerException(exception)}, trying to fix it by removing invalid chars");
+                var base64Chars = RemoveInvalidBase64Chars(base64Encoded);
+
+                try
+                {
+                    return Convert.FromBase64String(base64Chars);
+                }
+                catch (Exception)
+                {
+                    Logger.WriteToLog("Base64 decoding still failed returning empty byte array");
+                    return new byte[0];
+                }
 	        }
 	    }
 
@@ -54,5 +64,41 @@ namespace MsgReader.Mime.Decode
 	        }
 	    }
 	    #endregion
+
+        #region RemoveInvalidBase64Chars
+        /// <summary>
+        /// Removes everything from a base64 string that does not belong in it
+        /// </summary>
+        /// <param name="base64Encoded"></param>
+        /// <returns></returns>
+        private static string RemoveInvalidBase64Chars(string base64Encoded)
+        {
+            var result = new StringBuilder();
+
+            foreach (var chr in base64Encoded)
+            {
+                var val = (int) chr;
+                if (val >= 65 && val <= 90 ||	// 'A'..'Z'
+                    val >= 97 && val <= 122 ||	// 'a'..'z'
+                    val >= 48 && val <= 57 ||   // '0'..'9'
+                    val == 43 || val == 47)     // '+' and '/'
+                    result.Append(chr);
+                else
+                    Logger.WriteToLog($"Removing char with value '{val}' from base64 string");
+            }
+
+            // Add the correct amount of padding to cleaned string
+            var str = result.ToString();
+
+            // Remove padding first if there is any
+            str = str.TrimEnd('=');
+
+            var padding = str.Length % 4;
+            // Add correct amount of padding
+            str += padding % 2 > 0 ? "=" : "==";
+
+            return str;
+        }
+        #endregion
     }
 }
