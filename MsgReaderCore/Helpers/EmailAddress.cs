1730b735928e8c9cc0b394cdb8a32d213ac11ec6
Kees <magic-sessions@home.nl>
2021-11-17 07:55:32 +0100

    Add line

--- MsgReaderCore/Helpers/EmailAddress.cs
+++ MsgReaderCore/Helpers/EmailAddress.cs
@@ -34,6 +34,7 @@ namespace MsgReader.Helpers
     internal static class EmailAddress
     {
         private static readonly Regex EmailRegex = new Regex(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
         #region IsEmailAddressValid
         /// <summary>
         /// Return true when the E-mail address is valid
