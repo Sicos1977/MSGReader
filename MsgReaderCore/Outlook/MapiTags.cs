dd083fc1bbfd2b9da55c5153881e4a5aa453fe81
David Claeys <dcl@centexbel.be>
2024-05-22 12:55:46 +0200

    update wrong value

--- MsgReaderCore/Outlook/MapiTags.cs
+++ MsgReaderCore/Outlook/MapiTags.cs
@@ -450,7 +450,7 @@ internal static class MapiTags
     public const string PR_DELEGATION = "007E";
     public const string PR_TNEF_CORRELATION_KEY = "007F";
 
-    public const string PR_ICON_INDEX = "007F";
+    public const string PR_ICON_INDEX = "1080";
 
     /*
      *	Message content properties
