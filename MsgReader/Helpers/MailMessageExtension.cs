a9a95a39a00de76e8d70aae0886a90aea3225165
Besbes Souhaieb <Souhaieb.Besbes@fr.ch>
2026-04-23 16:15:28 +0200

    support .Net 10 and adapt to the removal of the internel Send method on MailMessage type

--- MsgReader/Helpers/MailMessageExtension.cs
+++ MsgReader/Helpers/MailMessageExtension.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net.Mail;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MsgReader.Helpers;
 
@@ -14,10 +16,10 @@ internal static class MailMessageExtension
     /// <param name="mail"><see cref="MailMessage"/></param>
     /// <param name="stream">The stream to write to</param>
     /// <exception cref="Exception"></exception>
-    internal static void WriteTo(this MailMessage mail, Stream stream)
+    internal static async Task WriteTo(this MailMessage mail, Stream stream)
     {
         var assembly = typeof(SmtpClient).Assembly;
-        
+
         var mailWriterType = assembly.GetType("System.Net.Mail.MailWriter") ??
                              throw new Exception("Failed to find internal constructor for MailWriterType");
 
@@ -27,10 +29,21 @@ internal static class MailMessageExtension
                                     throw new Exception("Failed to find internal constructor for MailWriter");
 
         var mailWriter = mailWriterConstructor.Invoke([stream, true]);
+#if (NET10_0_OR_GREATER)
+        var syncReadWriteAdapterType = assembly.GetType("System.Net.SyncReadWriteAdapter") ??
+                                       throw new Exception("Failed to find internal type SyncReadWriteAdapter");
+
+        var tokenSource = new CancellationTokenSource();
+        var sendMethod = typeof(MailMessage).GetMethod("SendAsync", BindingFlags.Instance | BindingFlags.NonPublic) ??
+                        throw new Exception("Failed to find internal 'SendAsync' method on MailMessage");
+
+        var genericSendMethod = sendMethod.MakeGenericMethod(syncReadWriteAdapterType);
+        await (Task)genericSendMethod.Invoke(mail, BindingFlags.Instance | BindingFlags.NonPublic, null, [mailWriter, true, true, tokenSource.Token], null!);
+#else
         var sendMethod = typeof(MailMessage).GetMethod("Send", BindingFlags.Instance | BindingFlags.NonPublic) ??
                          throw new Exception("Failed to find internal 'Send' method on MailMessage");
-        
         sendMethod.Invoke(mail, BindingFlags.Instance | BindingFlags.NonPublic, null, [mailWriter, true, true], null!);
+#endif
 
         var closeMethod = mailWriter.GetType().GetMethod("Close", BindingFlags.Instance | BindingFlags.NonPublic);
 
@@ -41,7 +54,7 @@ internal static class MailMessageExtension
                 new Exception("Failed to find internal 'Close' method on MailWriter");
 
         return;
-#elif(NETFRAMEWORK)
+#elif (NETFRAMEWORK)
         var mailWriterConstructor =
             mailWriterType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, [typeof(Stream)], null) ??
             throw new Exception("Failed to find internal constructor for MailWriter");
@@ -49,7 +62,7 @@ internal static class MailMessageExtension
         var mailWriter = mailWriterConstructor.Invoke([stream]);
         var sendMethod = typeof(MailMessage).GetMethod("Send", BindingFlags.Instance | BindingFlags.NonPublic) ??
                          throw new Exception("Failed to find internal 'Send' method on MailMessage");
-        
+
         sendMethod.Invoke(mail, BindingFlags.Instance | BindingFlags.NonPublic, null, [mailWriter, true, true], null!);
 
         var closeMethod = mailWriter.GetType().GetMethod("Close", BindingFlags.Instance | BindingFlags.NonPublic);
